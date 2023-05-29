639a5e86703afbcd5cd8a5918b1887219d3231a1
Braham7396 <brahamaggarwal739625@gmail.com>
2023-05-29 17:08:51 +0530

    Prepared App for deployment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -104,6 +105,9 @@ app.use(
     ],
   })
 );
+
+app.use(compression());
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNCAUGHT EXCEPTION! SHUTTING DOWN...');
   process.exit(1);
 });
 
@@ -31,8 +29,6 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNHANDLED REJECTION! SHUTTING DOWN...');
   server.close(() => {
     process.exit(1);
   });
