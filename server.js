9996499c3851cb56aaa22981dfab4a817ae8a6d6
Braham7396 <brahamaggarwal739625@gmail.com>
2023-05-29 17:40:20 +0530

    fixed port bug

--- server.js
+++ server.js
@@ -23,9 +23,8 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
-const port = 8000;
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+const server = app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${process.env.PORT}`);
 });
 
 process.on('unhandledRejection', (err) => {
